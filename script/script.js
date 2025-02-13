f522ffeebb5e269d725a353a263274db13dcb856
Keny 1 <keny.almazan@mrm.com>
2025-02-13 00:12:25 -0600

    Second commit - Data layers implementation

---
+++ script/script.js
@@ -0,0 +1,240 @@
+var contador;
+
+const menu_option_1 = document.getElementById("menu-option-1");
+
+menu_option_1.addEventListener("click", () => {
+
+    dataLayer.push({
+        event: 'menu_option_click',
+        option_menu: "1"
+    })
+
+});
+
+const botones = document.querySelectorAll("div[class='contenedor-botones'] button[class^=button]");
+
+var longitudArregglo = botones.length;
+
+// alert(longitudArregglo);
+
+const banner_confirmacion = document.getElementById("banner-confirmacion");
+
+const boton_cierre = document.getElementById("boton-cierre");
+
+const seccion_uno = document.querySelector("section[class='seccion_uno']");
+
+boton_cierre.addEventListener('click', () => {
+
+banner_confirmacion.setAttribute("class", "banner-confirmacion hidden");
+
+    for(let i = 0; i < longitudArregglo; i++) {
+        let boton = botones[i];
+        boton.removeAttribute("disabled");
+    }
+
+});
+
+const imagenBanner = document.querySelector("div[class='contenedor-cuerpo-banner']").querySelector("div[class='contenedor-imagen-confirmacion']").querySelector("img[class='imagen-confirmacion']");
+
+const textoBanner = document.querySelector("div[class='contenedor-cuerpo-banner']").querySelector("div[class='contenedor-titulo-banner-confirmacion']").querySelector("span[class='titulo-banner-confirmacion']");
+
+    for(let i = 0; i < longitudArregglo; i++) {
+        let boton = botones[i];
+        let idBoton = boton.getAttribute("id");
+        
+        boton.addEventListener("click", (e) => {
+            if(idBoton === e.target.id) {
+
+                // alert(boton.innerText)
+
+                var productName = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedor-caracteristicas']").querySelector("div[class*='contenedorNombreProducto']").querySelector("span[class*='titulo-producto']").innerText;        
+                var productCode = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedorCodigoProducto']").querySelector("span[class^='codigo-producto']").innerText;
+                var productQuantity = parseInt(boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedorCantidad']").querySelector("span[class^='cantidad-producto']").innerText);
+                var productPrice = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedor-caracteristicas']").querySelector("div[class*='contenedorPrecio']").querySelector("span[class*='precio-producto']").innerText;        
+                var productCategory = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedorCategoria']").querySelector("span[class^='categoria']").innerText;        
+                var productPriceNumber = parseInt(productPrice);
+                var finalPrice = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedor-caracteristicas']").querySelector("div[class*='contenedorPrecioFinal']").querySelector("span[class^='precio-final']").innerText;        
+
+                    if(boton.innerText === "+") {
+
+                        contador = productQuantity + 1;
+
+                        var etiquetaCantidad = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class='contenedorCantidad']").querySelector("span[class*='cantidad-producto']");
+
+                        var etiquetaPrecioFinal = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class='contenedorPrecioFinal']").querySelector("span[class^='precio-final']");
+
+                        etiquetaCantidad.innerText = contador;
+
+                        finalPrice = contador * productPriceNumber;  
+
+                        etiquetaPrecioFinal.innerText = finalPrice;
+
+                    } 
+                    
+                    if (boton.innerText === "-") {
+
+                        contador = productQuantity - 1;
+
+                        var etiquetaCantidad = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class='contenedorCantidad']").querySelector("span[class*='cantidad-producto']");
+
+                        var etiquetaPrecioFinal = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class='contenedorPrecioFinal']").querySelector("span[class^='precio-final']");
+
+                        if(contador < 1) {
+
+                            contador = 1;
+
+                        }
+
+                        etiquetaCantidad.innerText = contador;
+
+                        finalPrice = contador * productPriceNumber;
+
+                        etiquetaPrecioFinal.innerText = finalPrice;
+                        
+                    }
+
+                    productName = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedor-caracteristicas']").querySelector("div[class*='contenedorNombreProducto']").querySelector("span[class*='titulo-producto']").innerText;        
+                    productCode = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedorCodigoProducto']").querySelector("span[class^='codigo-producto']").innerText;
+                    productQuantity = parseInt(boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedorCantidad']").querySelector("span[class^='cantidad-producto']").innerText);
+                    productPrice = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedor-caracteristicas']").querySelector("div[class*='contenedorPrecio']").querySelector("span[class*='precio-producto']").innerText;        
+                    productCategory = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedorCategoria']").querySelector("span[class^='categoria']").innerText;    
+                    productPriceNumber = parseInt(productPrice);
+                    finalPrice = boton.closest("div[class*='tarjeta-producto']").querySelector("div[class*='contenedor-caracteristicas']").querySelector("div[class*='contenedorPrecioFinal']").querySelector("span[class^='precio-final']").innerText;        
+
+                    // alert(productName);
+
+                    // alert(productCategory);
+
+                    // alert(productPriceNumber);
+    
+                    // alert(productCode);
+    
+                    // alert(productQuantity);
+
+                    // alert(finalPrice);
+
+                if(boton.innerText == "Add") {
+
+                dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+
+                dataLayer.push({
+                    event: "add_to_cart",
+                    ecommerce: {
+                    currency: "USD",
+                    value: finalPrice,
+                        items: [
+                            {
+                            item_id: productCode,
+                            item_name: productName,
+                            price: productPriceNumber,
+                            quantity: productQuantity,
+                            item_category: productCategory
+                            }
+                                ]
+                            }
+                        });
+
+                        for(let i = 0; i < longitudArregglo; i++) {
+                            boton = botones[i];
+                            boton.setAttribute("disabled",true)
+                        }
+
+                        imagenBanner.setAttribute("src", "images/Iconos/Icono producto agregado.png")
+
+                        textoBanner.innerText = "Product added to cart";
+                        
+                        banner_confirmacion.setAttribute("class", "banner-confirmacion visible");
+
+                } else if (boton.innerText == "Remove") {
+
+                    dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+    
+                    dataLayer.push({
+                        event: "remove_from_cart",
+                        ecommerce: {
+                        currency: "USD",
+                        value: finalPrice,
+                            items: [
+                                {
+                                item_id: productCode,
+                                item_name: productName,
+                                price: productPriceNumber,
+                                quantity: productQuantity,
+                                item_category: productCategory
+                                }
+                                    ]
+                                }
+                            });
+
+                            for(let i = 0; i < longitudArregglo; i++) {
+                                boton = botones[i];
+                                boton.setAttribute("disabled",true)
+                            }
+    
+                            imagenBanner.setAttribute("src", "images/Iconos/Icono producto removido.png")
+    
+                            textoBanner.innerText = "Product removed from cart";
+                            
+                            banner_confirmacion.setAttribute("class", "banner-confirmacion visible");
+    
+                    } else if (boton.innerText == "Payment") {
+
+                        function generacionAleatorio() {
+                           var fecha = new Date();
+                           var dia = fecha.getDate();
+                           var diaTexto = "" + dia;
+                           var mes = fecha.getMonth() + 1;
+                           var mesTexto = "" + mes;
+                           var anio = fecha.getFullYear();
+                           var anioTexto = "" + anio;
+                           var seg = fecha.getSeconds();
+                           var segTexto = "" + seg;
+                           var min = fecha.getMinutes();
+                           var minTexto = "" + min;
+                           var hor = fecha.getHours();
+                           var horTexto = "" + hor;
+                           var fechaTexto = diaTexto.concat(mesTexto,anioTexto,horTexto,minTexto,segTexto);
+                           return (parseInt(Math.random()*10000)) + fechaTexto;
+                       }
+
+                       const transactionId = generacionAleatorio();
+
+                       dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+       
+                       dataLayer.push({
+                           event: "purchase",
+                           ecommerce: {
+                           transaction_id: transactionId,
+                           currency: "USD",
+                           value: finalPrice,
+                               items: [
+                                   {
+                                   item_id: productCode,
+                                   item_name: productName,
+                                   price: productPriceNumber,
+                                   quantity: productQuantity,
+                                   item_category: productCategory
+                                   }
+                                       ]
+                                   }
+                               });
+   
+                               for(let i = 0; i < longitudArregglo; i++) {
+                                   boton = botones[i];
+                                   boton.setAttribute("disabled",true)
+                               }
+       
+                               imagenBanner.setAttribute("src", "images/Iconos/Icono transacción exitosa.png")
+       
+                               textoBanner.innerText = "Successful purchase";
+                               
+                               banner_confirmacion.setAttribute("class", "banner-confirmacion visible");
+       
+                       }
+
+
+            }
+        })
+
+    }
+
