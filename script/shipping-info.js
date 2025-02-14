c62438e6de435cbb646366098320d0c8f7b5d96c
Keny 1 <keny.almazan@mrm.com>
2025-02-14 05:52:02 -0600

    Twelfth commit - new page paths V1.2

--- script/shipping-info.js
+++ script/shipping-info.js
@@ -22,21 +22,21 @@ const finalPrice = document.querySelector("div[class*='contenedor-caracteristica
 const productImage = document.querySelector("div[class='contenedor-imagen-producto'] img");
 
 if(productCodeShipping == "10006") {
-    rutaImagen = "../imagenes/products/Aceite 20w50 Super Moto 4t 20w-50 Bidón De 946 Ml Mobil.png";
+    rutaImagen = "imagenes/products/Aceite 20w50 Super Moto 4t 20w-50 Bidón De 946 Ml Mobil.png";
 } else if(productCodeShipping == "10027") {
-    rutaImagen = "../imagenes/products/Pack X12 Aceites Para Motor 4t Mx 15w-50 Mobil Super.webp";
+    rutaImagen = "imagenes/products/Pack X12 Aceites Para Motor 4t Mx 15w-50 Mobil Super.webp";
 }else if(productCodeShipping == "10053") {
-    rutaImagen = "../imagenes/products/Mobil Delvac Modern 15w-40 Def Ext 19l Ci-4plus Mobil 126146.png";
+    rutaImagen = "imagenes/products/Mobil Delvac Modern 15w-40 Def Ext 19l Ci-4plus Mobil 126146.png";
 }else if(productCodeShipping == "10052") {
-    rutaImagen = "../imagenes/products/Mobil Delvac Legend 20w-50 Alto Kilometraje Mobil 127763.webp";
+    rutaImagen = "imagenes/products/Mobil Delvac Legend 20w-50 Alto Kilometraje Mobil 127763.webp";
 }else if(productCodeShipping == "10025") {
-    rutaImagen = "../imagenes/products/Pack X6 Anticongelante Mobil Coolant 33 Vida Extendida.webp";
+    rutaImagen = "imagenes/products/Pack X6 Anticongelante Mobil Coolant 33 Vida Extendida.webp";
 }else if(productCodeShipping == "10062") {
-    rutaImagen = "../imagenes/products/Aceite Super Synthetic 5w-30 5 Litros Mobil.webp";
+    rutaImagen = "imagenes/products/Aceite Super Synthetic 5w-30 5 Litros Mobil.webp";
 }else if(productCodeShipping == "10046") {
-    rutaImagen = "../imagenes/products/Aceite Super Trc-pro 20w-50 Api Sp Bidón De 5 Litros Mobil.webp";
+    rutaImagen = "imagenes/products/Aceite Super Trc-pro 20w-50 Api Sp Bidón De 5 Litros Mobil.webp";
 }else if(productCodeShipping == "10059") {
-    rutaImagen = "../imagenes/products/Aceite Para Motor 5w-50 Sintético 1 Fs X2 946ml Mobil.webp";
+    rutaImagen = "imagenes/products/Aceite Para Motor 5w-50 Sintético 1 Fs X2 946ml Mobil.webp";
 }
 
 productName.innerText = producNameShipping;
